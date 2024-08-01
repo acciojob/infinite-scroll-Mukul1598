29b8d78ae92dc9ef8d162344a95578a16faaf9cd
Mukul Singh <135323609+Mukul1598@users.noreply.github.com>
2024-08-01 15:06:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 //your code here!
+const list = document.getElementById("infi-list");
+
+function addListItems(count){
+	for(let i = 0; i<count; i++){
+		cont listItem = document.createElement("li");
+		listItem.textContent = `Item ${list.children.length + 1}`;
+		list.appendChild{listItem};
+	}
+}
+
+addListItems(10);
+
+list.addEventListener("scroll", function(){
+	if(this.scrollTop + this.clientHeight >= this.scrollHeight){
+		addListItems(2);
+	}
+})
 
